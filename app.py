b876d2f67054bf2e7ba13753ee9fed49ed65d87d
rzet595 <77562448+rzet595@users.noreply.github.com>
2021-01-17 08:13:00 +0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,20 @@
+import random
+import os
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+@app.route("/")
+def _index():
+    photo = random.choice(os.listdir("static"))
+    id = photo.replace(".jpg", '').replace('id', '')
+    return render_template("index.html", photo=photo, id=id)
+
+
+@app.route("/donate")
+def _donate():
+    return redirect("https://www.tinkoff.ru/rm/ivanov.vladislav82/fLBa057505")
+
+
+if __name__ == "__main__":
+    app.run(port=5050)
